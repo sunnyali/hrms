a3b5323bb1ecf880d6560e31e47b05439f92f8ba
Shujaat Ali <shujaatali101@gmail.com>
2013-06-02 04:07:43 +0500

    test

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
  <div id="footer">
             Copyright &copy; <a href="http://www.1800sunstaff.com/" target="_blank">Sunrise Staffing</a>. 2013 - <?php echo date('Y') ?> All rights reserved.
         </div> <!-- footer -->
-
-        
+	
+     <!-- Owais and Ali -->   
         <script src="<?php echo $checkpage->web ?>/js/jquery-1.10.1.min.js" type="text/javascript"> </script>
 		<script src="<?php echo $checkpage->web ?>/js/jquery-ui.js" type="text/javascript"> </script>
